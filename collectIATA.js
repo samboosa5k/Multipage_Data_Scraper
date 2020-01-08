5bbf2161cb9ea073849405844ebf91c40d3bace5
samboosa5k <43879542+samboosa5k@users.noreply.github.com>
2020-01-08 18:02:36 +0100

    Greatly improved code: creating airport lists based on location name isntead of IATA code

--- collectIATA.js
+++ collectIATA.js
@@ -1,4 +1,10 @@
 /* 
+    Collect IATA codes from Wikipedia
+    - Adapter design pattern
+    - DOMParser tip came from Stackoverflow: https://stackoverflow.com/questions/36631762/returning-html-with-fetch
+*/
+
+/* 
     Imports
     - node-fetch = so node can process the fetching
     - node 'fs' = so the files can be writtent to disk
@@ -10,13 +16,16 @@ const jsdom = require('jsdom');
 const { JSDOM } = jsdom;
 
 /* 
-    Collect IATA codes from Wikipedia
-    - Adapter design pattern
-    - DOMParser tip came from Stackoverflow: https://stackoverflow.com/questions/36631762/returning-html-with-fetch
+    Constants: these will never change
 */
 const alphabet = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 const url = "https://en.wikipedia.org/wiki/List_of_airports_by_IATA_code:_";
 
+/* 
+    Functions:
+    - Fetching
+    - Parsing
+*/
 //  collectHTML & parseHTML - LOOP START
 const collectHTML = async (letter) => {
     const response = await nFetch(url+letter);
@@ -45,42 +54,77 @@ const parseHTML = async (incoming, letter) => {
             }
         );
     }
+
     return rawJSON;
 }
 //  collectHTML & parseHTML - LOOP END
 
-//  Combine all the scraped data per page object
-const combineJSON = async (alphabet) => {
-    const combined = [];
-    for(let i = 0; i<2; i++){
-        const html = await collectHTML(alphabet[i]);
-        const json = await parseHTML(html, alphabet[i]);
-        combined.push(json);
-    }
-    return combined;
-}
 
 //  Make the data actually usable
 const genJSON = async (incoming) => {
     const raw = await incoming;
     const useable = JSON.stringify(raw);
+
     return useable;
 }
 
-//  Initiate
-const startProcess = async (alphabet) => {
+/* 
+    Processes:
+    - storeIATA = not a process or function, just a storage for combined json
+    - collectIATA = collects and writes list of airports to disk, grouped alphabetically based on IATA
+    - adaptIATA = filters/regroups the collected data, writing list of airports to disk, grouped alphabetically absed on Location
+*/
+const storeIATA = [];
+
+const collectIATA = async (alphabet, storage) => {
     for(let i = 0; i<alphabet.length; i++){
+        console.log('collectIATA status: ', `Collection started of ${alphabet[i]}`);
+        //  Collect & parse IATA based on ALphabet-letter
         const html = await collectHTML(alphabet[i]);
         const json = await parseHTML(html, alphabet[i]);
-        const useable = await genJSON(json);
+        
+        //  Push to storeIATA array for use by adaptIATA
+        storage.push(json);
 
-        // Write to disk
-        fs.appendFile(`db_IATA/${alphabet[i]}_airports.json`, useable, (err)=>{
+        //  Create usable IATA Grouped JSON
+        const iataGrouped = await genJSON(json);
+
+        // File-writing: IATA-grouped 
+        fs.appendFile(`db_IATA/${alphabet[i]}_airports.json`, iataGrouped, (err)=>{
             if (err) throw err;
             console.log('File saved = ', `db_IATA/${alphabet[i]}_airports.js`);
         })
     }
 }
 
-startProcess(alphabet);
+const adaptIATA = async (alphabet, storage) => {
+    //  Temp storage for Location Grouped JSON
+    let locationGrouped = {};
+    
+    console.log('adaptIATA status: ', 'IATA collection reached end, now split into location groups...');
+    
+    for(let i = 0; i<alphabet.length; i++){
+        //  Filter combined list and group alphabetically by location name
+        locationGrouped = await genJSON(storage[i].filter((obj)=>{
+            return obj.location.startsWith(alphabet[i]) === true;
+        }));
 
+        // File-writing: Location-grouped
+        fs.appendFile(`db_LOCATION/${alphabet[i]}_airports.json`, locationGrouped, (err)=>{
+            if (err) throw err;
+            console.log('File saved = ', `db_LOCATION/${alphabet[i]}_airports.js`);
+        })
+    }
+    
+}
+
+
+/* 
+    INIT
+*/
+const startProcess = async () => {
+    collectIATA(alphabet, storeIATA)
+        .then(() => { adaptIATA(alphabet, storeIATA) });
+}
+
+startProcess();
